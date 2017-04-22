4dec1099cab5be90d1418173b9a5b3920f707e7f
WAYNE1894\wayne <mose286778@gmail.com>
2017-04-22 11:22:41 +0800

    update

---
+++ js/animation.js
@@ -0,0 +1,12 @@
+  $(function(){
+
+  })
+
+    //gsap的部份
+    //https://greensock.com/get-started-js#easing 
+
+		var tween_left=TweenMax.to($("#left_hand"), 2, {rotation:"-12deg",transformOrigin:'100% 100%', repeat:-1,repeatDelay:0.1, yoyo:true, ease:Sine.easeOut});
+
+
+    //tween_left.pause()
+   
